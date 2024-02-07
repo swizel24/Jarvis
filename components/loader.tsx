8601ef0f430faf79a7803184b747a862062792ee
swizel24 <142387482+swizel24@users.noreply.github.com>
2024-02-07 10:55:27 +0530

    Update loader.tsx

--- components/loader.tsx
+++ components/loader.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export const Loader = () => {
   return (
     <div className="h-full flex flex-col gap-y-4 items-center justify-center">
-      <div className="w-10 h-10 relative animate-pulse">
+      <div className="w-10 h-10 relative animate-spin">
         <Image
           alt="Logo"
           src="/logo.jpg"
